f942478d5dc7000f252e05cb48dadb353f153a00
nyohoki <nyohoki@gmail.com>
2021-01-26 18:47:58 -0800

    Add files via upload (#2)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nyohoki_higure_0.1.0
+Factorio mod.
+Only works in Freeplay and Sandbox for now.
--- settings.lua
+++ settings.lua
@@ -1,9 +1,57 @@
-data:extend(
-    {
+data:extend
+({
+    {   -- toggle nightmod
         type = "bool-setting",
-        name = "twilight-on-off",
+        name = "darkness",
         order = "a",
+        setting_type = "runtime-global",
+        default_value = true
+    },
+    {   -- adjust amount of darkness during night
+        type = "int-setting",
+        name = "rgb-light",
+        order = "c",
+        setting_type = "runtime-global",
+        default_value = 255,
+        minimum_value = 0,
+        maximum_value = 255,
+    },
+    {   -- lamp light size
+        type = "int-setting",
+        name = "lamp-light-size",
+        order = "d",
         setting_type = "startup",
-        default_value = "true"
-    }
-)
+        default_value = 40,
+        minimum_value = 1,
+        maximum_value = 255,
+    },
+    {   -- lamp light intensity
+        type = "string-setting",
+        name = "lamp-light-intensity",
+        order = "e",
+        setting_type = "startup",
+        default_value = 0.9,
+        minimum_value = 0.1,
+        maximum_value = 1.0,
+        allowed_values = {"0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"},
+    },
+    {   -- player light size
+        type = "int-setting",
+        name = "player-light-size",
+        order = "e",
+        setting_type = "startup",
+        default_value = 25,
+        minimum_value = 1,
+        maximum_value = 255,
+    },
+    {   -- player light intensity
+        type = "string-setting",
+        name = "player-light-intensity",
+        order = "g",
+        setting_type = "startup",
+        default_value = 0.4,
+        minimum_value = 0.1,
+        maximum_value = 1.0,
+        allowed_values = {"0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"},
+    },
+})
