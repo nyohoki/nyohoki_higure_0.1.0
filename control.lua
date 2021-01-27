0f093c608c8a7005f05d2996a95cb721a6cd973f
nyohoki <nyohoki@gmail.com>
2021-01-26 18:47:13 -0800

    Add files via upload

--- control.lua
+++ control.lua
@@ -1,20 +1,37 @@
-
-script.on_init(function()
-if remote.interfaces["freeplay"]
-    then
-        script.on_event(defines.events.on_player_created, function(event)
-        local omote = game.surfaces.nauvis
-        omote.freeze_daytime = true
-        omote.daytime = 0.5
-        omote.min_brightness = 0.1
-        omote.brightness_visual_weights = { 255 / 255, 255 / 255, 255 / 255}
-        end)
-elseif remote.interfaces["sandbox"]
-    then
-        local omote = game.surfaces.nauvis
-        omote.freeze_daytime = true
-        omote.daytime = 0.5
-        omote.min_brightness = 0.1
-        omote.brightness_visual_weights = { 255 / 255, 255 / 255, 255 / 255}
+if not nyohoki
+then nyohoki = {} 
 end
+
+if settings.global["darkness"].value == true
+then darkness = true
+elseif settings.global["darkness"].value == false
+then darkness = false
+end
+
+function nyohoki.twilight()
+    if darkness == true
+    then twilight = true
+    end
+end
+
+function nyohoki.higure()
+    local omote = game.surfaces.nauvis
+    omote.min_brightness = 0.1
+    omote.daytime = 0.5
+    omote.freeze_daytime = true
+    local rgblight = settings.global["rgb-light"].value
+    omote.brightness_visual_weights = {r = rgblight, g = rgblight, b = rgblight}
+end
+
+script.on_init
+(function()
+    local ri = remote.interfaces
+    if ri["freeplay"]
+    or ri["sandbox"]
+    and twilight -- new parameter
+    then script.on_event(defines.events.on_player_created, 
+       function(event)
+            nyohoki.higure()
+        end)
+    end
 end)
--- data.lua
+++ data.lua
@@ -1,1 +1,34 @@
-require("overrides")
+data.raw.lamp["small-lamp"].light =
+{
+    intensity = settings.startup["lamp-light-intensity"].value,
+    size = settings.startup["lamp-light-size"].value,
+    color =
+    {
+        r = 255/255,
+        g = 255/255,
+        b = 255/255
+    }
+}
+data.raw.lamp["small-lamp"].light_when_colored =
+{
+    intensity = settings.startup["lamp-light-intensity"].value,
+    size = settings.startup["lamp-light-size"].value,
+    color =
+    {
+        r = 255/255,
+        g = 255/255,
+        b = 255/255
+    }
+}
+data.raw.character.character.light =
+{
+    minimum_darkness = 1,
+    intensity = settings.startup["player-light-intensity"].value,
+    size = settings.startup["player-light-size"].value,
+    color =
+    {
+        r = 255/255,
+        g = 255/255,
+        b = 255/255
+    }
+}
